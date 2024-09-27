411d37799c08a79606ce83f1128c5b847930232f
jonathanockert <ockert.j@northeastern.edu>
2024-09-27 12:32:56 -0700

    Lab2 and A2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -2,6 +2,8 @@ export default function Lab2() {
     return(
         <div id="wd-lab2">
             <h2>Lab 2</h2>
+            <h3>CSS, React, and Bootstrap</h3>
+            
         </div>
     )
 }
